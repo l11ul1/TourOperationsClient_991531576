85fd764b0c35c9ec98e5718a06b0817450a72694
Vladislav Mun <munv@sheridancollege.ca>
2020-12-18 16:37:30 -0500

    Add project files.

---
+++ TourOperationsClient_991531576/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TourOperationsClient_991531576.ServiceReference1;
+
+namespace TourOperationsClient_991531576.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        DBOperationsClient client = new DBOperationsClient("BasicHttpBinding_DBOperations");
+
+        public ActionResult ShowAll()
+        {
+            List<TourInfo> tourInfos = client.RetrieveTourInfo().ToList();
+            return View(tourInfos);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TourOperationsClient_991531576/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TourOperationsClient_991531576.MvcApplication" Language="C#" %>
